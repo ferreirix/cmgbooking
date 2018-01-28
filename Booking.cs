587d6128bc87603964ff15ce6998282c34410ed3
ferreiria <ferreira.s.joao@gmail.com>
2018-01-28 16:38:20 +0100

    v1 - book classes from config file

--- Booking.cs
+++ Booking.cs
@@ -36,6 +36,21 @@ namespace CMGBooker
             httpClient = new HttpClient(httpHandler);
         }
 
+        /// <summary>
+        /// Gets the list of classes to book in the same day next week.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<SportsClass> GetClassesToBook()
+        {
+            using (StreamReader r = new StreamReader("mybookings.json"))
+            {
+                string json = r.ReadToEnd();
+                var classes = JsonConvert.DeserializeObject<List<SportsClass>>(json)
+                    .Where(c => c.Day == DateTime.UtcNow.DayOfWeek);
+                return classes;
+            }
+        }
+
         public async Task PerformLogin()
         {
             log.Info("Attempting to login");
@@ -71,70 +86,85 @@ namespace CMGBooker
                 log.Info(cookie.Name + ": " + cookie.Value);
         }
 
-        public async Task Book()
+        public async Task Book(IEnumerable<SportsClass> classesToBook)
         {
-            UriBuilder builder = new UriBuilder(BaseUrl + "/members/planning");
+            var schedule = await GetSchedule();
+
+            var links = GetClassesLink(schedule, classesToBook);
+
+            if (links?.Count() > 0)
+            {
+                log.Info($"links for booking {string.Join("; ", links.ToArray())}");
+                log.Info($"booking...");
+                foreach (var link in links)
+                {
+                    var bookingResult = await httpClient.PostAsync(link, null);
+                    log.Info(bookingResult.StatusCode.ToString());
+                }
+            }
+            else
+            {
+                log.Info("Could not get links to book");
+            }
+        }
+
+        /// <summary>
+        /// Returns all the classes for the same day next week
+        /// </summary>
+        /// <returns></returns>
+        private async Task<dynamic> GetSchedule()
+        {
+            UriBuilder builder = new UriBuilder(BaseUrl + PlanningSubPath);
             builder.Query = "activity=.*&minhour=7&maxhour=23&day=7";
 
             log.Info("Requesting classes page");
             var classesPageResult = await httpClient.GetAsync(builder.Uri);
-
             log.Info(classesPageResult.StatusCode.ToString());
             var classesContent = await classesPageResult.Content.ReadAsStringAsync();
 
-            var link = GetClassLink(classesContent);
-            log.Info($"link for booking {link}");
-
-            if (!string.IsNullOrWhiteSpace(link))
-            {
-                log.Info($"booking...");
-                var bookingResult = await httpClient.PostAsync(link, null);
-                log.Info(bookingResult.StatusCode.ToString());
-            }
-        }
-
-        private string GetClassLink(string html)
-        {
             var htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(html);
+            htmlDoc.LoadHtml(classesContent);
 
-            //get the json schedules
+            //get the schedule from json 
             var script = htmlDoc.DocumentNode.SelectSingleNode("//script[contains(text(),'planningTableData')]");
             var data = script.InnerText.Replace("var planningTableData =", "").Trim('\r', '\n', ';', ' ');
-            log.Info(data);
+            log.Info(data.Substring(0, 300));
 
-            dynamic classes = JsonConvert.DeserializeObject(data);
-            var link = string.Empty;
-
-            foreach (var sportsClass in classes)
-            {
-                if ("BODY PUMP".Equals((string)sportsClass.activity.name))
-                {
-                    Console.WriteLine(sportsClass.activity.name);
-                    Console.WriteLine(sportsClass.actions);
-                    var htmlLink = new HtmlDocument();
-                    //load html button with link to make a reservation
-                    htmlLink.LoadHtml((string)sportsClass.actions);
-                    link = htmlLink.DocumentNode.FirstChild.GetAttributeValue("data-subscribelink", "");
-                    break;
-                }
-            }
-            return link;
+            return JsonConvert.DeserializeObject(data);
         }
 
         /// <summary>
-        /// Gets the list of classes to book in the same day next week.
+        /// returns the url that allows to make the reservation
         /// </summary>
+        /// <param name="schedule"></param>
+        /// <param name="classesToBook"></param>
         /// <returns></returns>
-        public IEnumerable<SportsClass> GetClassesToBook()
+        private IEnumerable<string> GetClassesLink(dynamic schedule, IEnumerable<SportsClass> classesToBook)
         {
-            using (StreamReader r = new StreamReader("mybookings.json"))
+            var bookingLinks = new List<string>();
+
+            foreach (var classToBook in classesToBook)
             {
-                string json = r.ReadToEnd();
-                var classes = JsonConvert.DeserializeObject<List<SportsClass>>(json)
-                    .Where(c => c.Day == DateTime.UtcNow.DayOfWeek);
-                return classes;
+                foreach (var sportsClass in schedule)
+                {
+                    //evaluate if sports class is in the list of bookings for today
+                    if (string.Equals((string)sportsClass.activity.name, classToBook.Name,
+                        StringComparison.InvariantCultureIgnoreCase)
+                        &&
+                        string.Equals((string)sportsClass.date.display, classToBook.Time,
+                        StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Console.WriteLine(sportsClass.activity.name);
+                        var htmlLink = new HtmlDocument();
+
+                        //load html button with link to make a reservation
+                        htmlLink.LoadHtml((string)sportsClass.actions);
+                        bookingLinks.Add(htmlLink.DocumentNode.FirstChild.GetAttributeValue("data-subscribelink", ""));
+                        break;
+                    }
+                }
             }
+            return bookingLinks;
         }
     }
 }
--- CMGClassBooker.cs
+++ CMGClassBooker.cs
@@ -2,23 +2,29 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using System.Linq;
 
 namespace CMGBooker
 {
     public static class CMGClassBooker
     {
         [FunctionName("CMGClassBooker")]
-        public async static Task Run([TimerTrigger("0 0 */1 * * *")]TimerInfo myTimer, TraceWriter log)
+        public async static Task Run([TimerTrigger("15 0 7 * * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
 
             var booker = new Booking(log);
 
-            await booker.PerformLogin();
+            var classesToBook = booker.GetClassesToBook();
 
-            await booker.PerformHomeRequest();
+            if (classesToBook?.Count() > 0)
+            {
+                await booker.PerformLogin();
 
-            await booker.Book();
+                await booker.PerformHomeRequest();
+
+                await booker.Book(classesToBook);
+            }
         }
     }
 }
